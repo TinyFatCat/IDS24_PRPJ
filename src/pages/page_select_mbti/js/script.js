5ebab3521841e02b0318a327adc72da42b7ff426
TinyFatCat <126849634+TinyFatCat@users.noreply.github.com>
2024-08-06 09:14:41 +0900

    페이지 추가, 상호작용 뼈대 설계, 최종 카드 디자인 설계

--- src/pages/page_select_mbti/js/script.js
+++ src/pages/page_select_mbti/js/script.js
@@ -1,3 +1,4 @@
+//I,E카드 정보 불러오기
 let exp_i = document.querySelectorAll('#mbti_i');
 let exp_i_small = document.getElementById('cards-i');
 let exp_e = document.querySelectorAll('#mbti_e');
@@ -33,12 +34,15 @@ document.addEventListener('DOMContentLoaded', function () {
       card.addEventListener('click', function () {
         let selectedCard = wrapper.querySelector('.card__small.selected');
 
-        // 클릭된 카드를 토글 선택
+        //클릭된 카드를 토글 선택
         card.classList.toggle('selected');
-        // 이미 선택된 카드가 있는지 확인하고 선택을 해제
+        //이미 선택된 카드가 있는지 확인하고 선택을 해제
         if (selectedCard && selectedCard !== card) {
           selectedCard.classList.remove('selected');
+          //클릭한 카드 내부 글자;정보를 가져오기
+          window.localStorage.setItem('mbti', '1');
         }
+        //결정하기 버튼 제어
         let btnDisableOff = document.getElementById('btnDisable');
         let btnEnableOn = document.getElementById('btnEnable');
         if (wrapper.querySelector('.card__small.selected')) {
